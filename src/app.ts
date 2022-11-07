b70a439122a87e6a1bf61a5d83de27fd04afb0aa
unknown <nd51917@fer.hr>
2022-11-07 21:20:58 +0100

    First commit

---
+++ src/app.ts
@@ -0,0 +1,112 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path'
+import https from 'https';
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import {Pool} from 'pg'
+import cookieParser from 'cookie-parser';
+import csrf from 'csurf'
+
+dotenv.config()
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.use("/styles",express.static(__dirname + "/styles"));
+app.set('view engine', 'ejs');
+
+app.use(cookieParser(process.env.SECRET));
+var csrfProtect = csrf({cookie: true})
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+const portServer = 4080
+
+const config = {
+  baseURL: 'localhost:4080',
+};
+
+const pool = new Pool()
+  
+app.get('/',(req, res) => {
+  res.redirect('/sql/safe')
+});
+
+app.get('/sql/safe', (req, res) => {
+  res.render('sqls', {
+    players: []
+  });
+});
+
+app.get('/sql/unsafe', (req, res) => {
+  res.render('sqlu', {
+    players: []
+  });
+});
+
+app.get('/csrf/safe', csrfProtect , (req, res) => {
+  res.render('csrfs', {
+    csrfToken: req.csrfToken(),
+    success: false
+  });
+});
+
+app.get('/csrf/unsafe', (req, res) => {
+  res.render('csrfu', {
+    success: false
+  });
+});
+
+app.get('/csrf/test', (req,res) => {
+  res.render('csrftest')
+});
+
+app.post('/sql/safe', urlencodedParser, async (req, res) =>{
+  var playersQueried
+  try{
+    const resp = await pool.query('SELECT * FROM players WHERE firstname=$1', [req.body.playerName])
+    playersQueried = resp.rows
+  }catch(err){
+    console.log(err)
+    playersQueried = []
+  }
+  res.render('sqls', {
+    players: playersQueried
+  });;
+})
+
+app.post('/sql/unsafe', urlencodedParser, async (req, res) =>{
+  var playersQueried
+  try{
+    const resp = await pool.query(`SELECT * FROM players WHERE firstname=\'${req.body.playerName}\'`)
+    playersQueried = resp.rows
+  }catch(err){
+    console.log(err)
+    playersQueried = []
+  }
+  res.render('sqlu', {
+    players: playersQueried
+  });;
+})
+
+app.post('/csrf/safe', urlencodedParser, csrfProtect, (req, res) => {
+  res.render('csrfs', {
+    csrfToken: req.csrfToken(),
+    success: true
+  });
+});
+
+
+app.post('/csrf/unsafe', (req, res) => {
+  res.render('csrfu', {
+    success: true
+  });
+});
+
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+  }, app)
+  .listen(portServer, function () {
+    console.log(`Server running at https://localhost:${portServer}/`);
+});
